feeb99ef5e743f0ac3ce17c9ebc0c19047fbfde3
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2023-12-22 21:08:21 +0700

    update header

--- giao-tiep-nguoi-may/personal-web/js/script.js
+++ giao-tiep-nguoi-may/personal-web/js/script.js
@@ -1,19 +1,19 @@
 window.addEventListener('load', () => {
     let url = window.location.pathname;
     switch (url) {
-        case '/index.html':
+        case '/project-at-school/giao-tiep-nguoi-may/personal-web/index.html':
             document.querySelectorAll('[href="./index.html"]')[1].classList.add('active')
             break;
-        case '/about.html':
+        case '/project-at-school/giao-tiep-nguoi-may/personal-web/about.html':
             document.querySelector('[href="./about.html"]').classList.add('active')
             break;
-        case '/lab4.html':
+        case '/project-at-school/giao-tiep-nguoi-may/personal-web/lab4.html':
             document.querySelector('[href="./lab4.html"]').classList.add('active')
             break;
-        case '/lab5.html':
+        case '/project-at-school/giao-tiep-nguoi-may/personal-web/lab5.html':
             document.querySelector('[href="./lab5.html"]').classList.add('active')
             break;
-        case '/lab6.html':
+        case '/project-at-school/giao-tiep-nguoi-may/personal-web/lab6.html':
             document.querySelector('[href="./lab6.html"]').classList.add('active')
             break;
 
